ce56f307ebaaad09ffb1a9855290f0b10eba833e
nicolacardi <nicola.cardi@gmail.com>
2020-08-14 15:00:12 +0200

    color tabs

--- src/app/tickets/tickets-tabs/tabs.page.ts
+++ src/app/tickets/tickets-tabs/tabs.page.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['./../tickets.scss'],
 })
 export class TabsPage {
 
