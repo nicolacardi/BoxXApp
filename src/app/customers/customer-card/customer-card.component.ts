1d94b5b9e35bdd03c5266afba02b271cda434cdd
nicolacardi <nicola.cardi@gmail.com>
2020-08-29 10:13:44 +0200

    map

--- src/app/customers/customer-card/customer-card.component.ts
+++ src/app/customers/customer-card/customer-card.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterContentInit, ElementRef} from '@angular/core';
 import { customer } from 'src/app/_models/models';
 import { IonCardContent } from '@ionic/angular';
+
+declare var google;
 
 @Component({
   selector: 'app-customer-card',
   templateUrl: './customer-card.component.html',
   styleUrls: ['./../customers.scss'],
 })
-export class CustomerCardComponent implements OnInit {
+export class CustomerCardComponent implements OnInit, AfterContentInit {
 
   accordionExpanded = false;
+  map;
+  latlng: string;
+  latlngA;
+  @ViewChild("mapElement", { static: true }) mapelement;
 
-  // @ViewChild("cardContentDOM", { static: false }) cardContent: ElementRef;
   heightDynamic = 0;
   @Input()
   localCustomer :  customer;
@@ -19,16 +24,31 @@ export class CustomerCardComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    //console.log (this.cardContent);
   }
 
+  ngAfterContentInit() : void{
+    this.latlng = this.localCustomer.poi;
+    if (this.latlng != null) {
+      this.latlng = this.latlng.replace(" ", "");
+      let latlngA = this.latlng.split(',');
+      let lat = parseFloat(latlngA[0]);
+      let lng = parseFloat(latlngA[1]);
+      this.map = new google.maps.Map(
+        this.mapelement.nativeElement,
+        {
+          center: {lat: lat, lng: lng},
+          zoom: 8
+        }
+      );
+    }
+  }
 
   toggleAccordion() {
 
     if(this.accordionExpanded) {
       this.heightDynamic = 0;
     } else {
-      this.heightDynamic = 500;
+      this.heightDynamic = 800;
     }
     this.accordionExpanded = !this.accordionExpanded;
   }
